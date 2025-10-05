d03d8ba7b16176889eb92f8d6851450059c70956
Giovana <giovanadossantos107@gmail.com>
2025-10-04 21:34:44 -0300

    adicionando encapsulamento, construtores padrão e adicionados, atributos privados

--- src/animais/Cachorro.java
+++ src/animais/Cachorro.java
@@ -1,15 +1,74 @@
 package animais;
 
 public class Cachorro {
+    //Atributos
     //Definindo caracteristicas do cachorro
-    public String nome;
-    public String cor;
-    public int altura;
-    public double peso;
-    public int tamanhoDoRabo;
+    private String nome;
+    private String cor;
+    private int altura;
+    private double peso;
+    private int tamanhoDoRabo;
+
+    //Construtor padrão - recebe nada
+    public Cachorro(){}
+
+    //Construtores adicionados
+     public Cachorro(String nome, String cor, int altura, double peso, int tamanhoDoRabo, String estadoDeEspirito) {
+        this.nome = nome;
+        this.cor = cor;
+        this.altura = altura;
+        this.peso = peso;
+        this.tamanhoDoRabo = tamanhoDoRabo;
+        this.estadoDeEspirito = estadoDeEspirito;
+    }
 
     //Estado de espirito do cachorro
-    public String estadoDeEspirito;
+    private String estadoDeEspirito;
+
+    //Encapsulamento
+    public String getNome(){
+        return this.nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public int getTamanhoDoRabo() {
+        return tamanhoDoRabo;
+    }
+
+    public void setTamanhoDoRabo(int tamanhoDoRabo) {
+        this.tamanhoDoRabo = tamanhoDoRabo;
+    }
+
+    public String getEstadoDeEspirito() {
+        return estadoDeEspirito;
+    }
 
     //Métodos
     public void comer(){}
@@ -23,7 +82,16 @@ public class Cachorro {
     }
 
     public String interagir(String acao){
-        if (acao.equals("carinho")){
+        //Substituindo pelo swhitch
+        switch (acao) {
+            case "carinho" : this.estadoDeEspirito = "feliz"; break;
+            case "vai dormir!" : this.estadoDeEspirito = "bravo"; break;
+            case "pisar na patinha" : this.estadoDeEspirito = "triste"; break;
+
+            default: this.estadoDeEspirito = "neutro";
+        }
+
+       /* if (acao.equals("carinho")){
             this.estadoDeEspirito = "feliz";
             return estadoDeEspirito;
         }else if (acao.equals("vai dormir!")) {
@@ -31,6 +99,7 @@ public class Cachorro {
         }else {
             this.estadoDeEspirito = "neutro";
         }
+        */
 
         return estadoDeEspirito;
     }
--- src/classNaoExecutaveis.java
+++ src/classNaoExecutaveis.java
@@ -3,29 +3,33 @@ import animais.Cachorro;
 public class classNaoExecutaveis {
     public static void main(String[] args) {
 
-        //Variavel de referencia
-        //Cria a variavel cachorro
+        // Variavel de referencia
+        // Cria a variavel cachorro
         Cachorro cachorro1 = new Cachorro();
+        Cachorro cachorro2 = new Cachorro("Zion", "marrom",10, 5.5, 15, "nada");
 
-        //Definindo um cachorro
-        cachorro1.nome = "Zoe";
-        cachorro1.cor = "branco";
-        cachorro1.peso = 8.5;
-        cachorro1.tamanhoDoRabo = 10;
+        System.out.println(cachorro2.getNome());
+        System.out.println(cachorro2.getAltura());
 
-        //Endereço na memoria sobre o cachorro
+        // Definindo um cachorro
+        /*cachorro1.setNome("Zoe");
+        cachorro1.setCor("Branco");
+        cachorro1.setAltura(10);
+        cachorro1.setPeso(8.5);
+        cachorro1.setTamanhoDoRabo(10); */
+
+        // Endereço na memória sobre o cachorro
         System.out.println(cachorro1);
 
-        //Retorna meu print
+        // Retorna meu print
         cachorro1.latir();
 
-        //Retorna quando pega a bolinha
+        // Retorna quando pega a bolinha
         System.out.println("O cachorro pegou uma " + cachorro1.pegar());
 
-        //Estado de espirito
-        System.out.println("O cachorro está "+ cachorro1.interagir( "carinho"));
-        System.out.println("O cachorro está "+ cachorro1.interagir( "vai dormir!"));
-        System.out.println("O cachorro está "+ cachorro1.interagir( "nada"));
-
+        // Estado de espírito
+        System.out.println("O cachorro está " + cachorro1.interagir("carinho"));
+        System.out.println("O cachorro está " + cachorro1.interagir("vai dormir!"));
+        System.out.println("O cachorro está " + cachorro1.interagir("nada"));
     }
 }
